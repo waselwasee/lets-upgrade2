ae9e28909722ee56d45c61796ce19efffc05372e
waselwasee <98586898+waselwasee@users.noreply.github.com>
2022-01-28 14:31:35 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+let email = "stu@letsupgrade.com";
+let password = "12345";
+email = prompt("what's the email address");
+password = prompt("What's the password?");
+if(email=="stu@letsupgrade.com" & password=="12345"){
+    alert("access granted!");
+}
+else{
+    alert("access denied!");
+}
+
